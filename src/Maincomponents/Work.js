5152e12a90b482e28035f60c2eebf7b7b62d13a4
“Charmaine270302p”  <charmainemwandiringa@gmail.com>
2023-12-06 18:53:08 +0200

    adding styles  on the work section

--- src/Maincomponents/Work.js
+++ src/Maincomponents/Work.js
@@ -1,5 +1,6 @@
 import { useState,useRef } from 'react';
 import AnimatedLetters from '../Components/AnimatedLetters';
+import WorkImage from '../Components/WorkImage';
 
 const Work = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -17,13 +18,13 @@ const Work = () => {
         <p>Here you will find some of the personal and clients projects that I  developed!</p>
     </div>
 
-    <div className="work-gallery">
-        <img className="workfirst" src= 'images/Luis.png'  alt="workfirstimage"/>
-        <img className="workfirst" src= 'images/Bakersinn.png'  alt="worksecondimage"/>
-        <img className="workfirst" src= 'images/Directhome.png'  alt="workforthimage"/>
-        <img className="workfirst" src= 'images/Tay.png'  alt="workthirdimage"/>
-        <img className="workfirst" src= 'images/Load.png'  alt="workforthimage"/>
-        <img className="workfirst" src= 'images/resellme.png'  alt="workfiveimage"/>
+<div className="work-gallery">
+      <WorkImage src="images/Directhome.png" alt="workthirdimage" link="https://charmaine-directhome.netlify.app" />
+      <WorkImage src="images/Bakersinn.png" alt="worksecondimage" link="https://bakercharmy.vercel.app/" />
+      <WorkImage src="images/Load.png" alt="workforthimage" link="https://charmaine-loadhaul.netlify.app" />
+      <WorkImage src="images/resellme.png" alt="workfifthimage" link="https://reselle-project-charmy.vercel.app" />
+      <WorkImage src="images/nowall.png" alt="worksixthimage" link="https://charmaine-nowalls.netlify.app" />
+      <WorkImage src="images/mobi_pic.png" alt="workfirstimage" link="https://charmaine-mobi.netlify.app" />
     </div>
    
   
